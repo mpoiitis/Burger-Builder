32f52f24f2e0c8209631746699810fbe12d980b5
Marinos Poiitis <marinospoet@gmail.com>
2018-05-30 17:36:25 +0300

    add order button disabling and enabling

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -14,7 +14,6 @@ import * as actionTypes from '../../store/actions';
 class BurgerBuilder extends Component {
 
     state = {
-        purchasable: false,
         purchasing: false,
         loading: false,
         error: false
@@ -39,10 +38,7 @@ class BurgerBuilder extends Component {
             .reduce((sum, el) => {
                 return sum + el;
             }, 0);
-
-        this.setState({
-            purchasable: sum > 0
-        });
+        return sum > 0;
     }
 
     purchaseHandler = () => {
@@ -90,7 +86,7 @@ class BurgerBuilder extends Component {
                         disabled={disabledInfo}
                         price={this.props.price}
                         ordered={this.purchaseHandler}
-                        purchasable={this.state.purchasable} />
+                        purchasable={this.updatePurchaseState(this.props.ings)} />
                 </Aux>
             );
             
