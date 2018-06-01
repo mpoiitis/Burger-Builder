85441c43bb39efd665a938063f543d330c643341
Marinos Poiitis <marinospoet@gmail.com>
2018-06-01 12:36:28 +0300

    enable redux thunk for asynchronous code

--- src/index.js
+++ src/index.js
@@ -5,10 +5,15 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import burgerBuilder from './store/reducers/burgerBuilder';
+import thunk from 'redux-thunk';
 
-const store = createStore(burgerBuilder, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(burgerBuilder, composeEnhancers(
+    applyMiddleware(thunk)
+));
 
 const app = (
     <Provider store={store}>
