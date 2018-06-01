eaeafd08e75b8994f061793f9a2a03e2afab2131
Marinos Poiitis <marinospoet@gmail.com>
2018-06-01 14:37:32 +0300

    add redirect after burger order

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -3,6 +3,7 @@ import { Route, Redirect } from 'react-router-dom';
 import ContactData from './ContactData/ContactData';
 import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
 import { connect } from 'react-redux';
+import * as actions from '../../store/actions/index';
 
 class Checkout extends Component {
 
@@ -15,10 +16,12 @@ class Checkout extends Component {
     }
 
     render() {
-        let summary = <Redirect to='/' />
+        let summary = <Redirect to='/' /> 
         if (this.props.ings) {
+            const purchasedRedirect = this.props.purchased ? <Redirect to='/' /> : null;
             summary = (
                 <div> 
+                    {purchasedRedirect}
                     <CheckoutSummary
                         ingredients={this.props.ings}
                         checkoutCancelled={this.checkoutCancelledHandler}
@@ -35,7 +38,8 @@ class Checkout extends Component {
 
 const mapStateToProps = state => {
     return {
-        ings: state.burgerBuilder.ingredients
+        ings: state.burgerBuilder.ingredients,
+        purchased: state.order.purchased
     }
 }
 
--- src/store/actions/order.js
+++ src/store/actions/order.js
@@ -35,3 +35,9 @@ export const purchaseBurgerStart = () => {
         type: actionTypes.PURCHASE_BURGER_START
     };
 };
+
+export const purchaseInit = () => {
+    return {
+        type: actionTypes.PURCHASE_INIT
+    };
+};
