73248e5770b9ddbcda860f9f6dc78e93db53e84d
Marinos Poiitis <marinospoet@gmail.com>
2018-06-05 23:08:29 +0300

    fix test message

--- src/components/Navigation/NavigationItems/NavigationItems.test.js
+++ src/components/Navigation/NavigationItems/NavigationItems.test.js
@@ -23,7 +23,7 @@ describe('<NavigationItems />', () => {
         expect(wrapper.find(NavigationItem)).toHaveLength(3);
     });
 
-    it('should render three <NavigationItem /> elements if authenticated', () => {
+    it('should find a specific <NavigationItem /> element if authenticated', () => {
         wrapper.setProps({isAuthenticated: true});
         expect(wrapper.contains(<NavigationItem link="/logout">Logout</NavigationItem>)).toEqual(true);
     });
