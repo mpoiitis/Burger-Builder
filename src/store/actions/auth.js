fcb75d5aa88660ae1e44efee941c1ede2cd662be
Marinos Poiitis <marinospoet@gmail.com>
2018-06-07 15:11:01 +0300

    add sign in and sign up utility

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,5 +1,7 @@
 import * as actionTypes from './actionTypes';
 
+import axios from 'axios';
+
 export const authStart = () => {
     return {
         type: actionTypes.AUTH_START
@@ -20,8 +22,26 @@ export const authFail = (error) => {
     };
 };
 
-export const auth = (email, password) => {
+export const auth = (email, password, isSignup) => {
     return dispatch => {
         dispatch(authStart());
+        const authData = {
+            email: email,
+            password: password,
+            returnSecureToken: true
+        };
+        let url = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyCvxBHqYXIFvxa6nqOIhe8vx7az67J-pdI';
+        if (!isSignup) {
+            url = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=AIzaSyCvxBHqYXIFvxa6nqOIhe8vx7az67J-pdI';
+        }
+        axios.post(url, authData)
+            .then(response => {
+                console.log(response);
+                dispatch(authSuccess(response.data.idToken, response.data.localId));
+            })
+            .catch(err => {
+                console.log(err);
+                dispatch(authFail(err));
+            });
     };
 };
