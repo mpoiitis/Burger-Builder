440886843cd4af3cedef6f7b94d79af5d9d04a74
Marinos Poiitis <marinospoet@gmail.com>
2018-06-07 17:26:56 +0300

    change nav items wheter user is logged in or not

--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -5,6 +5,8 @@ import classes from './Layout.css';
 import Toolbar from '../../components/Navigation/Toolbar/Toolbar';
 import SideDrawer from '../../components/Navigation/SideDrawer/SideDrawer';
 
+import { connect } from 'react-redux';
+
 class Layout extends Component {
 
     state = {
@@ -24,8 +26,11 @@ class Layout extends Component {
     render() {
         return (
             <Aux>
-                <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler}/>
+                <Toolbar 
+                    isAuth = {this.props.isAuthenticated}
+                    drawerToggleClicked={this.sideDrawerToggleHandler}/>
                 <SideDrawer 
+                    isAuth = {this.props.isAuthenticated}
                     open={this.state.showSideDrawer}
                     closed={this.sideDrawerClosedHandler}/>
                 <main className={classes.Content}>
@@ -35,4 +40,11 @@ class Layout extends Component {
         )
     }
 }
-export default Layout;
+
+const mapStateToProps = state => {
+    return {
+        isAuthenticated: state.auth.token !== null
+    };
+};
+
+export default connect(mapStateToProps)(Layout);
